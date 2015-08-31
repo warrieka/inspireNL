e370333b304293a1c5b05ce921323bce9efe2279
warrieka <kaywarrie@gmail.com>
2015-08-31 23:08:43 +0200

    added datacatalog from geopunt

---
+++ i18n/nl.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl" sourcelanguage="nl">
+<context>
+    <name>inspireNL</name>
+    <message>
+        <location filename="inspireNL.py" line="164"/>
+        <source>&amp;INSPIRE Nederland plugin voor QGIS</source>
+        <translation>&amp;INSPIRE Nederland plugin voor QGIS</translation>
+    </message>
+    <message>
+        <location filename="inspireNL.py" line="154"/>
+        <source>INSPIRE NL</source>
+        <translation>INSPIRE NL</translation>
+    </message>
+</context>
+<context>
+    <name>inspireNLdlg</name>
+    <message>
+        <location filename="ui_inspireNL_dialog.py" line="52"/>
+        <source>INSPIRE Nederland plugin voor QGIS</source>
+        <translation>INSPIRE Nederland plugin voor QGIS</translation>
+    </message>
+    <message>
+        <location filename="ui_inspireNL_dialog.py" line="53"/>
+        <source>PushButton</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
---
+++ ui_inspireNL_dialog.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui_inspireNL_dialog.ui'
+#
+# Created: Mon Aug 31 22:09:17 2015
+#      by: PyQt4 UI code generator 4.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_inspireNLdlg(object):
+    def setupUi(self, inspireNLdlg):
+        inspireNLdlg.setObjectName(_fromUtf8("inspireNLdlg"))
+        inspireNLdlg.resize(400, 300)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/plugins/inspireNL/images/wrench.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        inspireNLdlg.setWindowIcon(icon)
+        self.verticalLayout = QtGui.QVBoxLayout(inspireNLdlg)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.textEdit = QtGui.QTextEdit(inspireNLdlg)
+        self.textEdit.setObjectName(_fromUtf8("textEdit"))
+        self.verticalLayout.addWidget(self.textEdit)
+        self.button_box = QtGui.QDialogButtonBox(inspireNLdlg)
+        self.button_box.setOrientation(QtCore.Qt.Horizontal)
+        self.button_box.setStandardButtons(QtGui.QDialogButtonBox.Close)
+        self.button_box.setObjectName(_fromUtf8("button_box"))
+        self.verticalLayout.addWidget(self.button_box)
+
+        self.retranslateUi(inspireNLdlg)
+        QtCore.QObject.connect(self.button_box, QtCore.SIGNAL(_fromUtf8("accepted()")), inspireNLdlg.accept)
+        QtCore.QObject.connect(self.button_box, QtCore.SIGNAL(_fromUtf8("rejected()")), inspireNLdlg.reject)
+        QtCore.QMetaObject.connectSlotsByName(inspireNLdlg)
+
+    def retranslateUi(self, inspireNLdlg):
+        inspireNLdlg.setWindowTitle(_translate("inspireNLdlg", "INSPIRE Nederland plugin voor QGIS", None))
+
+import resources_rc

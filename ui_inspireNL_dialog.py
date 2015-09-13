c1d92267324f715969a448938a1caaa168432c5e
warrieka <kaywarrie@gmail.com>
2015-09-13 11:14:00 +0200

    documentation

--- ui_inspireNL_dialog.py
+++ ui_inspireNL_dialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_inspireNL_dialog.ui'
 #
-# Created: Sun Sep 13 00:40:40 2015
+# Created: Sun Sep 13 00:42:04 2015
 #      by: PyQt4 UI code generator 4.11.2
 #
 # WARNING! All changes made in this file will be lost!

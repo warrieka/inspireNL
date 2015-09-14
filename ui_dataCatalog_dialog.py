02bd72724c4629c3f53064fdb1d66a7f31d5b6e5
warrieka <kaywarrie@gmail.com>
2015-09-14 22:43:33 +0200

    v0.2

--- ui_dataCatalog_dialog.py
+++ ui_dataCatalog_dialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_dataCatalog_dialog.ui'
 #
-# Created: Mon Sep 14 22:23:39 2015
+# Created: Mon Sep 14 22:38:52 2015
 #      by: PyQt4 UI code generator 4.11.2
 #
 # WARNING! All changes made in this file will be lost!
